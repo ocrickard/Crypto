733a2f4604709018f6550b8bf1692be5c1dd4aff
Oliver Rickard <ocrickard@gmail.com>
2018-06-23 16:46:56 -0700

    Completed challenge 4

---
+++ challenge4/target/debug/challenge4.d
@@ -0,0 +1,1 @@
+/Users/ocrickard/src/rust/crypto/challenge4/target/debug/challenge4.dSYM: /Users/ocrickard/src/rust/crypto/challenge4/src/main.rs
---
+++ challenge4/target/debug/deps/challenge4-c858286db4047cff.d
@@ -0,0 +1,5 @@
+/Users/ocrickard/src/rust/crypto/challenge4/target/debug/deps/challenge4-c858286db4047cff: src/main.rs
+
+/Users/ocrickard/src/rust/crypto/challenge4/target/debug/deps/challenge4-c858286db4047cff.d: src/main.rs
+
+src/main.rs:
